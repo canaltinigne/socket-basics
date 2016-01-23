e94a930786756523d5981a564b7f73e3ac9d7ef6
Can Altinigne <canaltinigne@gmail.com>
2016-01-23 23:49:19 +0200

    Added custom message event

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,7 @@ var socket = io();
 socket.on('connect', function (){
     console.log('Connected to Socket.io server!');
 });
+
+socket.on('message', function (message){
+    console.log('New message: ' + message.text);
+});
--- server.js
+++ server.js
@@ -6,12 +6,20 @@ var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
-io.on('connection', function () {
+io.on('connection', function (socket) {
     console.log('User connected via socket.io!');
+
+    socket.on('message', function (message) {
+        console.log('Message received: ' + message.text);
+        socket.broadcast.emit('message', message); //io.emit send everybody but yourself
+    });
+
+    socket.emit('message', {
+        text: 'Welcome to ChatApp ! '
+    });
 });
 
 
-http.listen(PORT, function (){
+http.listen(PORT, function () {
     console.log('Server started');
 });
-
