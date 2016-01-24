c6d0b245321569fe23d6be239762736f48cde006
Can Altinigne <canaltinigne@gmail.com>
2016-01-24 11:48:17 +0200

    Add front-end form to submit message

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,22 @@
 var socket = io();
 
-socket.on('connect', function (){
+socket.on('connect', function () {
     console.log('Connected to Socket.io server!');
 });
 
-socket.on('message', function (message){
+socket.on('message', function (message) {
     console.log('New message: ' + message.text);
+});
+
+var $form = jQuery('#message-form');
+
+$form.on('submit', function (Event) {
+    
+    Event.preventDefault();
+
+    socket.emit('message', {
+        text: $form.find('input[name=message]').val()
+    });
+    
+    $form.find('input[name=message]').val('');
 });
